a23953cf6675a23d365a42cb0df9af4b513be807
Wellington5000 <wellingtonsantos5000@gmail.com>
2022-01-03 00:42:58 -0300

    Crud e melhoramento de interface

--- view/src/app/home/home.component.ts
+++ view/src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { ModalNewBankComponent } from '../modal-new-bank/modal-new-bank.component';
+import * as globais from '../../eventosGlobais'
 /**
  * @title Basic use of `<table mat-table>`
  */
@@ -14,47 +15,61 @@ export class HomeComponent {
   codigo_compensacao: number | undefined
   nome_instituicao: string | undefined
   dataBank: any
-  displayedColumns = ['position', 'name'];
-  SERVER_URL = 'http://localhost:3000'
+  displayedColumns = ['position', 'name', 'actions'];
+  SERVER_URL = 'https://revgas-test.herokuapp.com/'
 
-  pesquisar(){
+  pesquisar() {
     //REQUISIÇÃO PARA LISTAR TODOS OS BANCOS
-    if(this.codigo_compensacao == undefined && this.nome_instituicao == undefined){
-      this.http.get(`${ this.SERVER_URL }/listagem_bancos`).subscribe((resultado) => {
+    if (this.codigo_compensacao == undefined && this.nome_instituicao == undefined) {
+      this.http.get(`${this.SERVER_URL}/listagem_bancos`).subscribe((resultado) => {
         this.dataBank = resultado
       });
     }
     //REQUISIÇÃO PARA BUSCAR PELO CÓDIGO
-    else if(this.codigo_compensacao != undefined && this.nome_instituicao == undefined){
-      this.http.get(`${ this.SERVER_URL }/consultar_codigo/${this.codigo_compensacao}`).subscribe((resultado) => {
+    else if (this.codigo_compensacao != undefined && this.nome_instituicao == undefined) {
+      this.http.get(`${this.SERVER_URL}/consultar_codigo/${this.codigo_compensacao}`).subscribe((resultado) => {
         this.dataBank = resultado
       });
       this.codigo_compensacao = undefined
     }
     //REQUISIÇÃO PARA BUSCAR PELO CÓDIGO E NOME
-    else if(this.codigo_compensacao != undefined && this.nome_instituicao != undefined){
-      this.http.get(`${ this.SERVER_URL }/consultar/${this.codigo_compensacao}/${this.nome_instituicao}`).subscribe((resultado) => {
+    else if (this.codigo_compensacao != undefined && this.nome_instituicao != undefined) {
+      this.http.get(`${this.SERVER_URL}/consultar/${this.codigo_compensacao}/${this.nome_instituicao}`).subscribe((resultado) => {
         this.dataBank = resultado
       });
       this.codigo_compensacao = undefined
       this.nome_instituicao = undefined
     }
     //REQUISIÇÃO PARA BUSCAR PELO NOME
-    else if(this.codigo_compensacao == undefined && this.nome_instituicao != undefined){
-      this.http.get(`${ this.SERVER_URL }/consultar_nome/${this.nome_instituicao}`).subscribe((resultado) => {
+    else if (this.codigo_compensacao == undefined && this.nome_instituicao != undefined) {
+      this.http.get(`${this.SERVER_URL}/consultar_nome/${this.nome_instituicao}`).subscribe((resultado) => {
         this.dataBank = resultado
       });
       this.nome_instituicao = undefined
     }
   }
 
-  constructor(private http: HttpClient, private dialog: MatDialog){ }
+  constructor(private http: HttpClient, private dialog: MatDialog) { }
 
-  novoBanco(){
+  novoBanco() {
     const dialogConfig = new MatDialogConfig()
     dialogConfig.disableClose = true
+    dialogConfig.data = { codigo_compensacao: '', nome_instituicao: '', salvar: true }
     dialogConfig.autoFocus = true
     this.dialog.open(ModalNewBankComponent, dialogConfig)
   }
- 
+
+  editarBanco(elemento: any) {
+    const dialogConfig = new MatDialogConfig()
+    dialogConfig.disableClose = true
+    dialogConfig.data = { codigo_compensacao: elemento.codigo_compensacao, nome_instituicao: elemento.nome_instituicao, salvar: false }
+    dialogConfig.autoFocus = true
+    this.dialog.open(ModalNewBankComponent, dialogConfig)
+  }
+
+  refreshEvento: any = null;
+
+  ngOnInit() {
+    this.refreshEvento = globais.EventEmitterService.get('refreshProdutos').subscribe(e => this.pesquisar());
+  }
 }
--- view/src/app/modal-new-bank/modal-new-bank.component.ts
+++ view/src/app/modal-new-bank/modal-new-bank.component.ts
@@ -1,7 +1,8 @@
 import { HttpClient, JsonpClientBackend } from "@angular/common/http";
-import { Component, OnInit } from "@angular/core";
+import { Component, Inject, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup } from "@angular/forms";
-import { MatDialogRef} from "@angular/material/dialog";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import * as globais from '../../eventosGlobais'
 
 @Component({
     selector: 'modal-new-bank',
@@ -11,26 +12,42 @@ import { MatDialogRef} from "@angular/material/dialog";
 export class ModalNewBankComponent implements OnInit {
     codigo_compensacao: number | undefined
     nome_instituicao: string | undefined
-    SERVER_URL = 'http://localhost:3000'
+    SERVER_URL = 'https://revgas-test.herokuapp.com/'
     form!: FormGroup;
 
-    save() {
-        console.log(this.codigo_compensacao, this.nome_instituicao)
-        this.http.post(`${this.SERVER_URL}/salvar`, {codigo_compensacao: this.codigo_compensacao, nome_instituicao: this.nome_instituicao}).subscribe((resultado) => {
+    salvar() {
+        this.http.post(`${this.SERVER_URL}/salvar`, { codigo_compensacao: this.data.codigo_compensacao, nome_instituicao: this.data.nome_instituicao }).subscribe((resultado) => {
             console.log(resultado)
         })
+        globais.EventEmitterService.get('refreshProdutos').emit(true);
+        this.dialogRef.close();
+    }
+
+    atualizar() {
+        this.http.post(`${this.SERVER_URL}/atualizar`, { codigo_compensacao: this.data.codigo_compensacao, nome_instituicao: this.data.nome_instituicao }).subscribe((resultado) => {
+            console.log(resultado)
+        })
+        globais.EventEmitterService.get('refreshProdutos').emit(true);
+        this.dialogRef.close();
+    }
+
+    deletar() {
+        console.log('deletar')
+        this.http.post(`${this.SERVER_URL}/deletar`, { codigo_compensacao: this.data.codigo_compensacao }).subscribe((resultado) => {
+            console.log(resultado)
+        })
+        globais.EventEmitterService.get('refreshProdutos').emit(true);
         this.dialogRef.close();
     }
 
     constructor(
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<ModalNewBankComponent>,
-        private http: HttpClient
-        ) { }
+        private http: HttpClient,
+        @Inject(MAT_DIALOG_DATA) public data: any
+    ) { }
 
     ngOnInit() { }
-
-    
 
     close() {
         this.dialogRef.close();
